c35e1da09847f34eb27d2dc347a055bf237e76d3
Hezekiah <irochezekiah@gmail.com>
2026-05-17 12:40:31 +0100

    Update userManagementRouter.js

--- routers/userManagementRouter.js
+++ routers/userManagementRouter.js
@@ -2,12 +2,14 @@ const express = require("express");
 const route = express.Router();
 const authenticate = require("../middlewares/auth");
 const authorize = require("../middlewares/authorize");
-const canModifyProfile = require("../middlewares/modifyRole");
-const {getProfile, searchUsers, updateProfile, deleteProfile, updateUserRole, toggleBanUser} = require("../controllers/userController");
+const canModifyProfile = require("../middlewares/modifyProfile");
+const {getProfile, getMe, searchUsers, updateProfile, deleteProfile, updateUserRole, toggleBanUser} = require("../controllers/userController");
 
+
+route.get("/me", getMe);
 route.get("/profile/:_id", authenticate, getProfile);
 route.get("/profiles", authenticate, searchUsers);
-route.put("/profile/:_id", authenticate, canModifyProfile, updateProfile);
+route.put("/profile/:_id", authenticate, updateProfile);
 route.patch("/profile/:_id/role", authenticate, authorize("superAdmin"), updateUserRole);
 route.patch("/profile/:_id/ban", authenticate, authorize("superAdmin"), toggleBanUser);
 route.delete("/profile/:_id", authenticate, canModifyProfile, deleteProfile);
