ceaaadc9031549deb9eeccea77b464fa741b2850
Hezekiah <irochezekiah@gmail.com>
2026-05-17 10:32:54 +0100

    Update email transport configuration for nodemailer

--- middlewares/sendEmail.js
+++ middlewares/sendEmail.js
@@ -3,10 +3,13 @@ const nodemailer = require("nodemailer");
 const sendEmail = async ({ to, subject, html }) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail", // or use SMTP
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            family: 4, // FORCE IPv4
             auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS, // use app password
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
             },
         });
 
