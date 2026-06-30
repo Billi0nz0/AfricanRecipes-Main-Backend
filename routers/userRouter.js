5ecaed60712f01c95e698cf2a53f4bc3b0510123
Hezekiah <irochezekiah@gmail.com>
2026-06-30 06:36:17 +0100

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const route = express.Router();
 const {register, verifyEmail, login, logout, forgotPassword, resetPassword} = require("../controllers/userController");
+
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs
-  message: "Too many request, please try again"
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: {
+        message: "Too many registration attempts. Please try again in 15 minutes.",
+    },
 });
 
 
