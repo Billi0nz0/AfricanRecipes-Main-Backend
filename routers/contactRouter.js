2023c1b6725fb25fe4a0d42000624f8c50561a14
Hezekiah <irochezekiah@gmail.com>
2026-06-30 07:12:38 +0100

    Modify contact request rate limit message
    Updated rate limit message for contact requests.

--- routers/contactRouter.js
+++ routers/contactRouter.js
@@ -2,13 +2,17 @@ const express = require("express");
 const route = express.Router();
 const { contactUs } = require("../controllers/contactController");
 const rateLimit = require("express-rate-limit");
+
 const contactLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 5,
     standardHeaders: true,
     legacyHeaders: false,
+
     message: {
-        message: "Too many attempts. Please try again in 15 minutes.",
+        success: false,
+        message:
+            "Too many contact requests. Please try again in 15 minutes or use another contact method.",
     },
 });
 
