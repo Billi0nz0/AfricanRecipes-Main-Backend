2e6f5aed1f72e82ef1e8daabc059159b647bd243
Hezekiah <irochezekiah@gmail.com>
2026-06-30 05:02:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 require("dotenv").config();
 const connectDB = require("./config/db");
 
