493bcae49c79ed68081e6c6a567cc3cc48b4f82a
Hezekiah <irochezekiah@gmail.com>
2026-06-30 07:05:07 +0100

    Implement rate limiting for contact form
    Added rate limiting to contact form submissions.

--- routers/contactRouter.js
+++ routers/contactRouter.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const route = express.Router();
 const { contactUs } = require("../controllers/contactController");
+const rateLimit = require("express-rate-limit");
+const contactLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: {
+        message: "Too many attempts. Please try again in 15 minutes.",
+    },
+});
 
-route.post("/", contactUs);
+route.post("/", contactLimiter, contactUs);
 
 module.exports = route;
