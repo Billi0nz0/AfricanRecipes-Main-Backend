ec5cff0e3e568212b206d7554fb463473830d3c8
Hezekiah <irochezekiah@gmail.com>
2026-06-30 07:18:56 +0100

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,32 +4,35 @@ const {register, verifyEmail, login, logout, forgotPassword, resetPassword} = re
 
 const rateLimit = require("express-rate-limit");
 const regLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 15 * 60 * 1000,
     max: 5,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
+        success: false,
         message: "Too many registration attempts. Please try again in 15 minutes.",
     },
 });
 
 const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 15 * 60 * 1000,
     max: 10,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
+        success: false,
         message: "Too many login attempts. Please try again in 15 minutes.",
     },
 });
 
 const resetLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 15 * 60 * 1000,
     max: 3,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
-        message: "Too many requests. Please try again in 15 minutes.",
+        success: false,
+        message: "Too many password reset requests. Please try again in 15 minutes.",
     },
 });
 
